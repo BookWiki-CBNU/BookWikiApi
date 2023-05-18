d87e58efdf49429157762f06e307a9b4b47d891d
LEE026 <lhs456852@gmail.com>
2023-05-18 18:27:59 +0900

    feature: readBooksByTag 에서 tag가 null일 때 처리 제작

--- src/main/java/com/api/bigdata/service/BookService.java
+++ src/main/java/com/api/bigdata/service/BookService.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.json.simple.JSONObject;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Service
@@ -20,7 +22,7 @@ public class BookService {
     public List<JSONObject> readBooksByTag(BookTag tag){
         String strTag = tagMatching(tag);
         if(strTag==null){
-            return bookRepository.findBooksByKdcCode("810");
+            return bookRepository.findBooksByExcludeKdcCode(new ArrayList<>(Arrays.asList("600", "300", "500")));
         }
         else {
             return bookRepository.findBooksByKdcCode(strTag);
